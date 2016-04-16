fb1a6baf392b9862eddb33ea122ff63d6f118b11
KyraYee <kny02014@mymail.pomona.edu>
2016-04-16 07:20:47 -0700

    Update FileParser.java

--- FileParser.java
+++ FileParser.java
@@ -1,3 +1,5 @@
+package EnglishTeacher;
+
 import java.io.File;
 import java.util.ArrayList;
 
@@ -14,12 +16,12 @@ public ArrayList<ArrayList<String>>parseFile(File theFile){
 	}
 	return returnMe;
 }
-
+/*
 static public void main(String[] s){
 	FileParser theParse = new FileParser();
 	ArrayList<ArrayList<String>> yo = theParse.parseFile(new File("test.txt"));
 	for(int i=0; i< yo.size(); i++){
 		System.out.println(yo.get(i));
-	}
-}
+	}*/
+//}
 }
